bfdd355515288758201c847e33bbb7a555a01905
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-09-28 00:08:36 -0300

    chore: extract data from jwt

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtGuard } from './jwt.guard';
 
@@ -15,7 +15,8 @@ export class AuthController {
 
     @UseGuards(JwtGuard)
     @Get('test-auth')
-    test() {
+    test(@Req() req) {
+        console.log(req.user);
         return { message: 'test' };
     }
 
--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -27,6 +27,7 @@ export class AuthService {
         const payload = {
             sub: user.id,
             username: user.username,
+            role: user.role,
         };
 
         return this.jwtService.sign(payload);
